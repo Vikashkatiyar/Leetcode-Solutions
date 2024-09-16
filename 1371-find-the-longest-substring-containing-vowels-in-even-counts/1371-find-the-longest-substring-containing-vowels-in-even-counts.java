52387b4a631231b44eb3f2f5ab20e9e440da4f99
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2024-09-16 22:13:21 +0530

    Time: 44 ms (54.45%), Space: 45.3 MB (60.50%) - LeetHub

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.java
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -1,39 +1,34 @@
 class Solution {
+    // https://youtu.be/6Xf5LfM-ciI?si=hKkVagExozofjgL5
     public int findTheLongestSubstring(String s) {
-        HashMap<String, Integer> map=new HashMap<>();
-        String currState="00000"; //aeiou
+        HashMap<Integer, Integer> map=new HashMap<>(); //O(2^5)-> O(32)
+        int mask=0; //000000
         int maxLen=0;    
-        int[] state=new int[5];    
+          
             
-        map.put(currState, -1);
+        map.put(mask, -1);
         
         for(int i=0;i<s.length();i++){
             char ch= s.charAt(i);
             
             // ^ ->0 means even, 1 means odd 
             if(ch=='a'){             
-                state[0]=(state[0]^1);  
+                mask=(mask^(1<<0)); 
             }else if(ch=='e'){
-                state[1]=(state[1]^1);
+                mask=(mask^(1<<1));
             }else if(ch=='i'){
-                state[2]=(state[2]^1);
+                mask=(mask^(1<<2));
             }else if(ch=='o'){
-                state[3]=(state[3]^1);
+                mask=(mask^(1<<3));
             }else if(ch=='u'){
-                state[4]=(state[4]^1);
+                mask=(mask^(1<<4));
             }
             
             
-            currState="";
-            for(int j=0;j<5;j++){
-                currState+=state[j];
-            }
-            
-            
-            if(map.containsKey(currState)){
-                maxLen=Math.max(maxLen, i-map.get(currState));
+            if(map.containsKey(mask)){
+                maxLen=Math.max(maxLen, i-map.get(mask));
             }else{
-                map.put(currState, i);
+                map.put(mask, i);
             }
             
         }
