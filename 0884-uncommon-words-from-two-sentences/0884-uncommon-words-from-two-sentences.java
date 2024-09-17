54a2e895dcda159cac08e29b3eac6f36d05e7236
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2024-09-17 22:46:15 +0530

    Time: 7 ms (8.11%), Space: 42.3 MB (13.76%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.java
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.java
@@ -1,26 +1,20 @@
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
-        String []a=s1.split(" ");
-        String []b=s2.split(" ");
-        HashMap<String,Integer> map=new HashMap<>();
-        for(String s:a){
-            map.put(s,map.getOrDefault(s,0)+1);
-        }
-        for(String s:b){
-            map.put(s,map.getOrDefault(s,0)+1);
-        }
+        // Using HashMap to count occurrences of words
+        Map<String, Integer> wordCount=new HashMap<>();
         
-        ArrayList<String> list=new ArrayList<>();
-        for(String s:map.keySet()){
-            if(map.get(s)==1){
-                list.add(s);
-            }
-        }
-        String[] res = new String[list.size()];
-        int idx = 0;
-        for(String s:list){
-            res[idx++] = s;
-        }
-        return res;
+        // Splitting and counting words from both sentences
+        Arrays.stream((s1 + " " + s2).split(" "))
+              .forEach(word->wordCount.put(word, wordCount.getOrDefault(word, 0)+1));
+        
+        // Collecting words with a count of 1 (uncommon words)
+        List<String> uncommonWords=wordCount.entrySet()
+            .stream()
+            .filter(entry->entry.getValue()==1)
+            .map(Map.Entry::getKey)
+            .collect(Collectors.toList());
+        
+        // Converting the list of uncommon words to an array and returning
+        return uncommonWords.toArray(new String[0]);
     }
 }
